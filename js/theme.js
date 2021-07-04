073e34332cf4e587994e4aef2392fcadcbc218b3
NiktoX2 <byniktox2@gmail.com>
2021-07-04 19:25:28 +0300

    new

--- js/theme.js
+++ js/theme.js
@@ -1,20 +1,19 @@
-let theme = document.querySelector('.theme');
-let themeButton = document.querySelector('.theme-button');
+var css = document.getElementById("theme");
 
-let list = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
-let Button = document.querySelector('.theme-button');
+var favicon = document.getElementById("favicon");
 
-let test = 1;
+var test = 1
 
-themeButton.onclick = function() {
-	theme.classList.toggle('light-theme');
-	theme.classList.toggle('dark-theme');
-	
+function light(){
 	if (test === 1){
-		list.forEach(function(element) {element.setAttribute('href', 'images/iso/white.png');});
-	    test = 0;
-	}else{
-		list.forEach(function(element) {element.setAttribute('href', 'images/iso/ico-site.png');});
-	    test = 1;
+		css.href = "css/theme/light.css";
+		favicon.href = "images/favicon/favicon-white.png";
+		test = 0
 	}
-};
+	else
+	{
+		css.href = "css/theme/dark.css";
+		favicon.href = "images/favicon/favicon-black.png";
+		test = 1
+	}
+}
