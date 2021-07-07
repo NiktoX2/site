666fb92a6cf2c78b648493df5d4b90cca0cee5e8
NiktoX2 <byniktox2@gmail.com>
2021-07-07 14:05:13 +0300

    new

---
+++ js/game.js
@@ -0,0 +1,6 @@
+var test = document.getElementById("1");
+var test2 = document.getElementById("0");
+
+	function dl(){
+		document.getElementById("text").innerHTML = "Вы открываете файл";
+	}
---
+++ js/window.js
@@ -0,0 +1,3 @@
+//var w = window.innerWidth;
+var h = window.innerHeight;
+document.getElementById("main").style.minHeight = h+"px";
